5c7ed0ce2cadb2d8eacb4da58c3242de405236ae
snitz-t2 <snitz.t2@gmail.com>
2021-10-14 22:32:29 +0300

    fixed alignmens_slow, now has the same results as MATLAB; added test

--- python/tests/test_alignments.py
+++ python/tests/test_alignments.py
@@ -23,14 +23,12 @@ class TestAlignments(object):
         This test verifies that running the LSD algorithm on the test image returns exacly the same results as running
         it in MATLAB.
         """
-        # 0) load input data (image) and output data (algorithm results for all lines)
+        # 0) load input data (input points) and output data (line detections)
         points_in = np.loadtxt(join(data_folder, 'test__pyalignments_slow__points_in.txt'), delimiter=',')
+        detections_out = np.loadtxt(join(data_folder, 'test__pyalignments_slow__detections_out.txt'), delimiter=',')
 
         # 1) call the function
         detections = pyalignments_slow.detect_alignments(points_in)
 
-        # # 2) run the algorithm
-        # lines = lsd.run_lsd(image_in, cut_results=False)
-        #
-        # # 3) assert results are correct
-        # assert np.all(np.abs(lines - lines_out) < 1e-12)
+        # 3) assert results are correct
+        assert np.all(np.abs(detections_out - detections) < 1e-12)
