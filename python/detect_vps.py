e8ba176aa9fc2fc74b1e5d4015cfe1ad482580b7
snitz-t2 <snitz.t2@gmail.com>
2021-10-20 00:36:10 +0300

    wrote some auxilary functions for the algorithms + tests for some of them; main algorithm is finished with bugs and without graphs

--- python/detect_vps.py
+++ python/detect_vps.py
@@ -1,12 +1,15 @@
 import os
 import cv2
 import numpy as np
+from copy import deepcopy
 from LSD import LSDWrapper
 from matplotlib import pyplot as plt
 from denoise_lines import denoise_lines
 from PCLines import PCLines_straight_all
 from find_detections import find_detections
 from detect_vps_params import DefaultParams, RuntimeParams
+from scipy.linalg import null_space
+from scipy.spatial.distance import cdist
 
 
 class VanishingPointDetector:
@@ -76,18 +79,37 @@ class VanishingPointDetector:
         points_straight, points_twisted = self._convert_to_PClines(lines)
 
         # 7) run alignment detector in straight and twisted spaces
-        detections_straight, m1, b = find_detections(points_straight, self.default_params)
+        detections_straight, m1, b1 = find_detections(points_straight, self.default_params)
         detections_twisted, m2, b2 = find_detections(points_twisted, self.default_params)
 
         # 8) gather intial vanishing point detections (convert them from dual spaces)
+        mvp_all, NFAs = self._read_detections_as_vps(detections_straight, m1, b1,
+                                                     detections_twisted, m2, b2)
 
         # 9) refine detections
+        mvp_all = self._refine_detections(mvp_all, lines_lsd)
 
         # 10) draw preliminary detections
+        mvp_all, NFAs = self._remove_duplicates(mvp_all, NFAs)
 
-        # 11) compute horizon line
+        # 11) draw preliminary detections
 
-        # 12) draw dual spaces
+        # 12) compute horizon line
+        if mvp_all.size > 0:
+            if self.default_params.MANHATTAN:
+                horizon_line, vpimg = compute_horizon_line_manhattan(mvp_all, NFAs, lines_lsd)
+            else:
+                horizon_line, vpimg = compute_horizon_line_non_manhattan(mvp_all, NFAs, lines_lsd)
+        else:
+            horizon_line = np.array([])
+            vpimg = np.array([])
+            print("No vanishing points found")
+
+        # 13) draw dual spaces
+
+        # 14) finish algorithm
+        print("Finished")
+        return horizon_line, vpimg
 
     def _draw_lines(self, lines1: np.ndarray, lines2: np.ndarray) -> None:  #TODO: finish
         H = self.runtime_params.H
@@ -201,4 +223,186 @@ class VanishingPointDetector:
         :param lines_lsd:
         :return:
         """
-        pass
+        D = mvp_all.shape[1]
+        mvp_refined = np.zeros((D, 2))
+        for ii in range(D):
+            vp = mvp_all[:, ii].T
+            vp = self._refine_vp(lines_lsd, vp)
+            mvp_refined[ii, :] = vp
+
+        return mvp_refined
+
+    def _refine_vp(self, lines, vp):
+        """
+        Given a cluster of line segments, and two segments indicated by p1 and p2,
+        obtain the main vanishing point determined by the segments
+        :param lines:
+        :param vp:
+        :return:
+        """
+        # 0) get configured parameters from class
+        THRESHOLD = self.default_params.REFINE_THRESHOLD
+        H = self.runtime_params.H
+        W = self.runtime_params.W
+
+        # 1) copy the original vanishing point
+        vp_orig = deepcopy(vp)
+
+        # -------------------------- beginning of `refine_vp_iteration` function in MATLAB -----------------------------
+
+        # 2) find intersection of each line in cluster with vanishing point segments
+
+        mp = np.vstack([lines[:, 0] + lines[:, 2], lines[:, 1] + lines[:, 3]]).T / 2
+
+        L = lines.shape[0]
+        O = np.ones((L, 1))
+        vpmat = np.tile(vp, [L, 1])
+
+        VP = np.cross(np.hstack([mp, O]).T, np.hstack([vpmat, O]).T).T
+
+        VP3 = np.tile(VP[:, 2], [1, 3])
+        VP = VP / VP3
+
+        a = VP[:, 0]
+        b = VP[:, 1]
+
+        # 3) get angle between lines
+        angle = np.abs(np.arctan(-a / b) - np.arctan((lines[:, 3] - lines[:, 1]) / (lines[:, 2] - lines[:, 0])))
+        angle = np.min(angle, np.pi - angle)
+        z2 = angle < np.deg2rad(THRESHOLD)
+
+        # 4) obtain a refined VP estimate from sub-cluster z2
+        lengths = np.sum((lines[:, 2] - lines[:, 2:]) ** 2, axis=1)
+        weights = lengths / np.max(lengths)
+        lis = self.line_to_homogeneous(lines)
+
+        Is = np.diag([1, 1, 0])
+
+        l2 = lis[z2, :].T
+        w2 = weights[z2]
+        w2 = np.tile(w2, [1, 3]).T
+
+        b = np.dot(Is.T @ l2, l2).T
+        b = np.tile(b, [1, 3]).T
+        Q = (w2 * l2 / b) @ l2.T
+
+        p = np.array([[0, 0, 1]]).T
+        A = np.hstack([2 * Q, -p])
+
+        vp = null_space(A)
+
+        vp = (vp[:2, 0] / vp[2, 1]).T
+
+        # -------------------------- end of `refine_vp_iteration` function in MATLAB -----------------------------------
+
+        # 5) check how much the refined vp changed relatively to the original vp
+        variation = np.linalg.norm(vp - vp_orig) / np.linalg.norm(vp_orig)
+
+        if variation > self.default_params.VARIATION_THRESHOLD:
+            return vp_orig                       # vp changed too much, probably unstable conditions, keeping initial vp
+        else:
+            return vp
+
+    def _remove_duplicates(self, vps, NFAs):
+        """
+        Identifies and removes duplicate detections, keeping only most significant ones.
+        :param vps:
+        :param NFAs:
+        :return:
+        """
+        THRESHOLD = self.default_params.DUPLICATES_THRESHOLD
+        clus = self.aggclus(vps.T, THRESHOLD)
+
+        final_vps = np.empty((2, 0))
+        final_NFAs = np.array([])
+
+        for ii in range(len(clus)):
+            c = clus[ii]
+            if len(c) == 1:
+                final_vps = np.append(final_vps, vps[:, c])
+                final_NFAs = np.append(final_NFAs, NFAs[c])
+            else:
+                I = np.argmax(NFAs[c])
+                final_vps = np.append(final_vps, vps[:, c[I]])
+                final_NFAs = np.append(final_NFAs, NFAs[c[I]])
+
+        return final_vps, final_NFAs
+
+
+    @staticmethod
+    def aggclus(X: np.ndarray, THRESHOLD: float):
+        """
+        agglomerative clustering using single link.
+        TODO: finish doc
+        """
+        N = X.shape[0]
+
+        D = cdist(X, X)
+
+        n = np.sqrt(X[:, 0] ** 2 + X[:, 1] ** 2)[:, np.newaxis]
+        n = np.tile(n, [1, N])
+        n = np.maximum(n, n.T)      # max norm
+
+        D /= n
+        np.fill_diagonal(D, np.inf)
+
+        clus = [ [] for _ in range(N)]
+        for ii in range(N):
+            clus[ii] = [ii]
+
+        D_flat = D.flatten(order='F')
+        I = np.argmin(D_flat)
+        V = D_flat[I]
+
+        while V < THRESHOLD:
+            ii, jj = np.unravel_index(3, D.shape, order='F')
+
+            if ii > jj:
+                t = ii
+                ii = jj
+                jj = t
+
+            clus[ii] += clus[jj]
+            clus.pop(jj)
+
+            d = np.minimum(D[ii, :], D[jj, :])
+
+            D[ii, :] = d
+            D[:, ii] = d.T
+            D = np.delete(D, obj=jj, axis=0)
+            D = np.delete(D, obj=jj, axis=1)
+
+            D[ii, ii] = np.inf
+
+            D_flat = D.flatten(order='F')
+            I = np.argmin(D_flat)
+            V = D_flat[I]
+
+        return clus
+
+
+
+
+    @staticmethod
+    def line_to_homogeneous(l: np.ndarray) -> np.ndarray:
+        """
+        converts lines in x1 y1 x2 y2 format to homogeneous coordinates.
+        :param l:
+        :return:
+        """
+        x1 = l[:, 0]
+        y1 = l[:, 1]
+        x2 = l[:, 2]
+        y2 = l[:, 3]
+
+        dx = x2 - x1
+        dy = y2 - y1
+        a = -dy
+        b = dx
+        c = -a * x1 -b * y1
+
+        L = np.vstack([a, b, c]).T
+        return L
+
+
+
--- python/tests/test_VanishingPointDetector.py
+++ python/tests/test_VanishingPointDetector.py
@@ -14,6 +14,37 @@ from detect_vps import VanishingPointDetector
 
 
 class TestVanishingPointDetector(object):
+    def test__line_to_homogeneous(self):
+        # 0) load input data and output data
+        lines_in = np.loadtxt(join(data_folder, 'test__denoise_lines__lines_in.txt'), delimiter=',')
+        lines_homog_out = np.loadtxt(join(data_folder, 'test__line_to_homogeneous__lines_homog_out.txt'), delimiter=',')
+
+        # 1) call the function
+        lines_homog = VanishingPointDetector.line_to_homogeneous(lines_in)
+
+        # 2) assert correctness
+        assert np.all(np.abs(lines_homog - lines_homog_out) < 1e-9)
+
+    def test__aggclus(self):
+        # 0) load input data and output data
+        X_in = np.array([[800.894333585489,268.80800259091],
+                         [345.968434441128,18339.9731893547],
+                         [-949.74228744014,263.862166183153],
+                         [799.834641087275,268.829479610743],
+                         [408.125435891671,10760.8963609918]])
+        clus_out = [[0, 3], [1], [2], [4]]
+        thresh_in = 1e-2
+
+        # 1) call the function
+        clus = VanishingPointDetector.aggclus(X_in, thresh_in)
+
+        # 2) assert correctness
+        assert len(clus) == len(clus_out)
+        for ii in range(len(clus)):
+            assert len(clus[ii]) == len(clus_out[ii])
+            for jj in range(len(clus_out[ii])):
+                assert clus[ii][jj] == clus_out[ii][jj]
+
     def test__detect_vps(self):
 
         # 0) define input data (image)
